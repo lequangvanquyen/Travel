0e438396d02c364e496b7a530b265ac05cd9e25f
Van Quyen <your.email@example.com>
2023-06-06 10:12:31 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+/* scroll */
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', function () {
     header.classList.toggle('sticky', this.window.scrollY > 0);
 })
 
+/* menu */
 let menu = document.querySelector('#menu-icon');
 let navmenu = document.querySelector('.navmenu');
 
